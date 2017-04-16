524ce54e150511a2d526a8cc6a93a76af7621861
Miguel <lowreybadboy@gmail.com>
2017-04-16 17:22:32 +0200

    Anado autenticacion en app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
 
-
+handleAuth(){
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+    .catch(error => console.log(`Ha ocurrido un error: ${error.code}:${error.message}`));
+}
   
   render() {
     return (
@@ -12,6 +18,7 @@ class App extends Component {
           <h2>Pseudogram</h2>
         </div>
         <p className="App-intro">
+          <button onClick={this.handleAuth}>Log with google</button>
         </p>
       </div>
     );
