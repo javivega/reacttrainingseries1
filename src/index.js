0945951edb6c11d1ee0c8703f5efd478ab260f29
Miguel <lowreybadboy@gmail.com>
2017-04-16 11:07:14 +0200

    anado firebase config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
 
 
+firebase.initializeApp({
+  apiKey: "AIzaSyB0AAWwhyHuuyItALTKbQJ4e4apUoiGYq0",
+  authDomain: "pseudogram-21afb.firebaseapp.com",
+  databaseURL: "https://pseudogram-21afb.firebaseio.com",
+  projectId: "pseudogram-21afb",
+  storageBucket: "pseudogram-21afb.appspot.com",
+  messagingSenderId: "1050105365722"
+});
 
 
 ReactDOM.render(
