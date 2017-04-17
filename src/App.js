91ba788ba3ccd71d700ab6671371f3db7db768d1
Miguel <lowreybadboy@gmail.com>
2017-04-17 16:26:10 +0200

    implemento login y logout

--- src/App.js
+++ src/App.js
@@ -3,14 +3,55 @@ import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    }
 
-handleAuth(){
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-    .then(result => console.log(`${result.user.email} ha iniciado sesion`))
-    .catch(error => console.log(`Ha ocurrido un error: ${error.code}:${error.message}`));
-}
-  
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => { //onAuthStateChange me devuelve un objeto user
+      this.setState({
+        user: user
+      })
+    })
+  }
+
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesion`))
+      .catch(error => console.log(`Ha ocurrido un error: ${error.code}:${error.message}`));
+  }
+
+  handleLogOut(){
+    firebase.auth().signOut() //devuelve una promesa
+      .then(result => console.log(`${result.user.email} ha salido`))
+      .catch(error => console.log(`Ha ocurrido un error: ${error.code}:${error.message}`));
+  }
+
+  renderLoginButton() {
+    //Comprueba si el usuario esta logeado
+    if (this.state.user) {
+      return (
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+          <p>Hola {this.state.user.displayName} </p>
+          <button onClick={this.handleLogOut}>Logout</button>
+        </div>
+      );
+
+    } else {
+      return (
+        <button onClick={this.handleAuth}>Log with google</button>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,7 +59,7 @@ handleAuth(){
           <h2>Pseudogram</h2>
         </div>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>Log with google</button>
+          {this.renderLoginButton()}
         </p>
       </div>
     );
