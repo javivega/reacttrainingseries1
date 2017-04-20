89bd5180190d1c912de93506de6f7e7d4dfb9a6b
Miguel <lowreybadboy@gmail.com>
2017-04-20 15:49:54 +0200

    Final anadiendo imagenes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: null
+      user: null,
+      pictures: []
     }
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
   }
 
   componentWillMount() {
+    //Aqui tenemos un listener para la autenticacion, que cuando cambia el usuario autenticado hace lo siguiente.
     firebase.auth().onAuthStateChanged(user => {
       this.setState({
         user: user
+      })
+    })
+    //Igual que tenemos un listener para la autenticacion ponemos otro para la base de datos
+    firebase.database().ref('pictures').on('child_added', snapshot => {
+      this.setState({
+        //con concat cada vez que añadimos un hijo creamos un nuevo array con todo el contenido de la base de datos(snapshot)
+        pictures: this.state.pictures.concat(snapshot.val())
       })
     })
   }
@@ -33,14 +46,59 @@ class App extends Component {
 
   }
 
+  handleUpload(event){
+        //obtenemos el archivo que se ha subido
+        const file = event.target.files[0];
+        //definimos una referencia al storage de firebase
+        const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+        //para subir el fichero cojo la referencia del archivo en el storage y con el put subo el archivo
+        const task = storageRef.put(file);
+
+        //el task tiene un evento on el cual puede escuchar eventos uno de ellos es state_changed
+        //este evento devuelve un snapshot del storage en ese momento o de la subida
+        task.on('state_changed', snapshot => {
+            let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.setState({
+                uploadValue: percentage
+            })
+        }, error => {
+            console.log(error.message);
+        }, () => {
+          //Con esto de debajo tenemos el objeto que queremos almacenar en la base de datos
+            const record = {
+              photoURL: this.state.user.photoURL,
+              displayName: this.state.user.displayName,
+              image: task.snapshot.downloadURL
+            };
+
+            //ahora que tengo el objeto creado con los datos a almacenar lo almacenamos
+            const dbRef = firebase.database().ref('pictures')
+            //creamos el huevo en la base de datos para añadir el nuevo registro
+            const newPicture = dbRef.push();
+            //guardamos en el hueco el objeto creado.
+            newPicture.set(record);
+
+        })
+    }
+
   renderLoginButton() {
     if (this.state.user) {
       return (
         <div>
-          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>{this.state.user.displayName}</p>
           <button onClick={this.handleLogOut}>Logout</button>
-          <FileUpload />
+          <FileUpload onUpload={this.handleUpload}/>
+
+          {this.state.pictures.map(picture => (
+            <div>
+              <img src={picture.image} alt=""/>
+              <br/>
+              <img src={picture.photoURL} alt={picture.displayName}/>
+               <br/>
+               <span>{picture.displayName}</span>
+            </div>
+          )).reverse()}
         </div>
       )
     } else {
--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,45 +1,22 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
+
 
 class FileUpload extends Component {
     constructor(){
         super();
         this.state = {
-            uploadValue: 0,
-            picture: null
+            uploadValue: 0
         };
 
-    this.handleUpload = this.handleUpload.bind(this);
     }
     //Primero defino el archivo a subir luego la referencia de donde en el storage y lo subo con put
-    handleUpload(event){
-        //obtenemos el archivo que se ha subido
-        const file = event.target.files[0];
-        //definimos una referencia al storage de firebase
-        const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
-        //para subir el fichero cojo la referencia del archivo en el storage y con el put subo el archivo
-        const task = storageRef.put(file);
-
-        //el task tiene un evento on el cual puede escuchar eventos uno de ellos es state_changed
-        //este evento devuelve un snapshot del storage en ese momento o de la subida
-        task.on('state_changed', snapshot => {
-            let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            this.setState({
-                uploadValue: percentage
-            })
-        }, error => {
-            console.log(error.message);
-        }, this.setState({
-            uploadValue: 100,
-            picture: task.snapshot.downloadURL
-        }) )
-    }
+    
 
     render(){
         return(
             <div>
                 <progress value={this.state.uploadValue} max="100"></progress>
-                <input type="file"  onChange={this.handleUpload}/>
+                <input type="file"  onChange={this.props.onUpload}/>
                 <img width="320" src={this.state.picture} alt="Imagen"/>
             </div>
         )
